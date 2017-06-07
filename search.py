77879e1cfe20cc0145aba9c3259bb0167822bf8f
wallyssonlima <wallyssonlds@gmail.com>
2017-06-07 11:36:59 -0300

    inital scripts

---
+++ search.py
@@ -0,0 +1,50 @@
+import urllib3
+import json
+
+
+TOKEN = 'kREqlf230Nve2axC11agMfTiDtRbAFxOcvJzrRIRUu7yFg9Ag3R0YDW8IZjtIZtJ'
+AUTHORIZATION = 'Basic MjAxNTExNDQwMTAzMjE6bTMwLkIwMDkxMDkzNTYyMDBi'
+
+
+def informacoes_basicas_aluno(matricula):
+    http = urllib3.PoolManager()
+    global TOKEN
+    r = http.request(
+        'GET',
+        'https://suap.ifrn.edu.br/api/v2/edu/alunos/{}/'.format(matricula),
+        headers={'Accept': 'application/json',
+                 'X-CSRFToken': TOKEN,
+                 'Authorization': AUTHORIZATION,
+                 }
+    )
+
+    return json.loads(r.data.decode('utf-8'))
+
+
+def informacoes_basicas_servidor(token_servidor, matricula):
+    http = urllib3.PoolManager()
+    global TOKEN, AUTHORIZATION
+    r = http.request(
+        'GET',
+        'https://suap.ifrn.edu.br/api/v2/edu/servidores/{}/'.format(matricula),
+        headers={'Accept': 'application/json',
+                 'X-CSRFToken': token_servidor,
+                 'Authorization': AUTHORIZATION,
+                 }
+    )
+
+    return json.loads(r.data.decode('utf-8'))
+
+
+def informacoes_curso(id_curso):
+    http = urllib3.PoolManager()
+    global TOKEN
+    r = http.request(
+        'GET',
+        'https://suap.ifrn.edu.br/api/v2/edu/cursos/{}/'.format(id_curso),
+        headers={'Accept': 'application/json',
+                 'X-CSRFToken': TOKEN,
+                 }
+    )
+
+    return json.loads(r.data.decode('utf-8'))
